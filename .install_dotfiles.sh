0fd5e006b9c8bc581b12fb6043ac7a3134c447d6
Gabriel Cuendet <gabriel.cuendet@gmail.com>
2018-08-24 14:36:51 +0200

    Add the steps to clone the bare repo and setup config

--- .install_dotfiles.sh
+++ .install_dotfiles.sh
@@ -1,3 +1,18 @@
+git clone --bare https://github.com/gcuendet/dotfiles.git $HOME/.dotfiles
+function config {
+   /usr/bin/git --git-dir=$HOME/.dotfiles/ --work-tree=$HOME $@
+}
+mkdir -p .config-backup
+config checkout
+if [ $? = 0 ]; then
+  echo "Checked out config.";
+  else
+    echo "Backing up pre-existing dot files.";
+    config checkout 2>&1 | egrep "\s+\." | awk {'print $1'} | xargs -I{} mv {} .config-backup/{}
+fi;
+config checkout
+config config status.showUntrackedFiles no
+
 # Install YouCompleteMe (needs to compile dependencies)
 # ---
 cd ~/.vim/pack/gabriel/start/youcompleteme
